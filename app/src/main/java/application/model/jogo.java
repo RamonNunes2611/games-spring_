827b8d91d42d8a0fde0c09a64f00558bf52439fe
RamonNunes2611 nunes <ramonnunes.oliva@gmail.com>
2024-09-12 00:01:22 +0000

    salvar

--- app/src/main/java/application/model/jogo.java
+++ app/src/main/java/application/model/jogo.java
@@ -3,12 +3,14 @@ package application.model;
 import java.util.HashSet;
 import java.util.Set;
 
-
 import jakarta.persitence.Column;
 import jakarta.persitence.Entity;
 import jakarta.persitence.GeneratedValues;
 import jakarta.persitence.GerantionType;
 import jakarta.persitence.Id;
+import jakarta.persitence.joinColumn;
+import jakarta.persitence.JoinTable;
+import jakarta.persitence.ManyToMany;
 import jakarta.persitence.OneToMany;
 import jakarta.persitence.Table;
 
@@ -20,14 +22,25 @@ public class Jogo{
     private long id;
     @Column (unique = true , nullable = false)
     private String titulo;
+
+    @ManyToOne
+    @joinColumn(name = "id_genero")
+    private String categoria;
+
+    @ManyToMany
+    @Jointable(
+        name = "jogos_possuem_plataformas",
+        joinColumns = @joinColumn(name = "id_jogos"),
+        inverseJoinColumnns = @joinColumn(name = "id_plataformas"))
+    private Set<Plataforma>plataformas = new HashSet<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
     
-public void setId(long id) {
-
-}public long getId() {
-    return id;
-}
-
-
-public String getTitulo() {
-    return titulo;
-}
+ 
